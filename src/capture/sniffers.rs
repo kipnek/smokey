edb918a9d8a85719db16e2828ebc0db528ffd7d3
sigh-gone <alexwhite1964@icloud.com>
2023-10-07 19:56:00 -0500

    changes to sniffer

--- src/capture/sniffers.rs
+++ src/capture/sniffers.rs
@@ -1,5 +1,4 @@
 use crate::packet_objects::basics::BasePacket;
-use circular_buffer::CircularBuffer;
 use pcap::Device;
 use std::collections::VecDeque;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -10,68 +9,17 @@ use std::{panic, thread};
 for gui implementation
  */
 #[derive(Debug, Clone, Default)]
-pub struct LiveCap {
-    pub interface: Vec<String>,
-    pub live_buffer: Arc<Mutex<CircularBuffer<30, BasePacket>>>,
-    pub stop: Arc<AtomicBool>,
-}
-
-impl LiveCap {
-    pub fn new() -> Self {
-        LiveCap {
-            interface: vec![],
-            live_buffer: Arc::new(Mutex::new(CircularBuffer::new())),
-            stop: Arc::new(AtomicBool::new(false)),
-        }
-    }
-    pub fn live_capture(&mut self) {
-        //, interfaces:Vec<String>) {
-        let stop = self.stop.clone();
-        let live_buffer = self.live_buffer.clone();
-        thread::spawn(move || {
-            let mut index = 0;
-
-            //only for development
-            let device = Device::lookup()
-                .and_then(|dev_result| match dev_result {
-                    Some(dev) => Ok(dev),
-                    None => Err(pcap::Error::PcapError("no device".to_string())),
-                })
-                .unwrap_or_else(|err| panic!("Device lookup failed: {}", err));
-
-            if let Ok(mut cap) =
-                pcap::Capture::from_device(device).and_then(|cap| cap.immediate_mode(true).open())
-            {
-                while let Ok(packet) = cap.next_packet() {
-                    if stop.load(Ordering::Relaxed) {
-                        break;
-                    }
-                    if let Ok(mut buffer_lock) = live_buffer.lock() {
-                        buffer_lock.push_back(BasePacket::new(index, packet.data.to_vec()));
-                        index += 1;
-                    }
-                }
-                stop.store(false, Ordering::Release);
-                drop(live_buffer);
-            }
-        });
-    }
-    pub fn stop(&mut self) {
-        self.stop.store(true, Ordering::Relaxed);
-    }
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct CliCap {
+pub struct LiveCapture {
     pub interfaces: Vec<String>,
-    pub cli_buffer: Arc<Mutex<VecDeque<BasePacket>>>,
+    pub cli_buffer: Arc<Mutex<VecDeque<Vec<BasePacket>>>>,
     pub stop: Arc<AtomicBool>,
 }
 
-impl CliCap {
+impl LiveCapture {
     pub fn capture(&mut self) -> Result<(), String> {
         let stop = self.stop.clone();
         let cli_buffer = self.cli_buffer.clone();
+        let mut vec_indexer = 0;
         thread::spawn(move || {
             let mut index = 0;
 
@@ -91,7 +39,11 @@ impl CliCap {
                         break;
                     }
                     if let Ok(mut buffer_lock) = cli_buffer.lock() {
-                        buffer_lock.push_back(BasePacket::new(index, packet.data.to_vec()));
+                        if buffer_lock[vec_indexer].len() >= 1000{
+                            buffer_lock.push_back(vec![]);
+                            vec_indexer+=1;
+                        }
+                        buffer_lock[vec_indexer].push(BasePacket::new(index, packet.data.to_vec()));
                         index += 1;
                     }
                 }
