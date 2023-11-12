8e5585ede5b2fa1d92f2e64eb2614872cffd2e80
sigh-gone <alexwhite1964@icloud.com>
2023-11-11 21:38:28 -0600

    got a pane to populate

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,13 @@ use eframe;
 use eframe::Frame;
 use egui;
 use egui::{Context, ScrollArea, Ui, WidgetText};
+use egui_tiles::{Behavior, TileId, UiResponse};
 use std::panic;
 use std::time::Duration;
-use egui_tiles::{Behavior, TileId, UiResponse};
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         //drag_and_drop_support: true,
-
         initial_window_size: Some([1280.0, 1024.0].into()),
 
         #[cfg(feature = "wgpu")]
@@ -32,33 +31,32 @@ fn main() -> Result<(), eframe::Error> {
 pub struct Capture {
     pub running: bool,
     pub sniffer: Sniffer,
-    pub tree : egui_tiles::Tree<Pane>,
+    pub tree: egui_tiles::Tree<Pane>,
 }
 struct TreeBehavior<'a> {
     captured_packets: &'a Vec<EthernetFrame>,
     drilldown: &'a String,
     payload: &'a Vec<u8>,
 }
+#[derive(Clone)]
 pub struct Pane {
     title: String,
     module: Module,
 }
 
-pub struct Drilldown{
-    pub info : String,
+#[derive(Clone)]
+pub struct Drilldown {
+    pub info: String,
 }
 
-impl Drilldown{
-    pub fn new()->Self{
-        Self{
+impl Drilldown {
+    pub fn new() -> Self {
+        Self {
             info: "".to_string(),
         }
     }
-    pub fn render(){
-
-    }
+    pub fn render() {}
 }
-
 
 impl eframe::App for Capture {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
@@ -66,6 +64,14 @@ impl eframe::App for Capture {
             self.get_packets();
             ctx.request_repaint_after(Duration::from_secs(1));
         }
+        egui::SidePanel::left("tree").show(ctx, |ui| {
+            if ui.button("Start").clicked() {
+                self.start();
+            }
+            if ui.button("Stop").clicked() {
+                self.stop();
+            }
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
             let mut behavior = TreeBehavior {
                 captured_packets: &self.sniffer.captured_packets,
@@ -75,12 +81,19 @@ impl eframe::App for Capture {
             self.tree.ui(&mut behavior, ui);
         });
         /*egui::CentralPanel::default()
-            .frame(egui::Frame::dark_canvas(&ctx.style()))
-            .show(ctx, |ui| self.ui(ui));*/
+        .frame(egui::Frame::dark_canvas(&ctx.style()))
+        .show(ctx, |ui| self.ui(ui));*/
     }
 }
 impl<'a> Behavior<Pane> for TreeBehavior<'a> {
-    fn pane_ui(&mut self, _ui: &mut Ui, _tile_id: TileId, _pane: &mut Pane) -> UiResponse {
+    fn pane_ui(&mut self, ui: &mut Ui, _tile_id: TileId, pane: &mut Pane) -> UiResponse {
+        match pane.module.clone() {
+            Module::Packets(mut table) => {
+                table.render(ui, &self.captured_packets);
+            }
+            Module::PacketDrill(_) => {}
+            Module::Payload => {}
+        }
         UiResponse::None
     }
 
@@ -89,13 +102,12 @@ impl<'a> Behavior<Pane> for TreeBehavior<'a> {
     }
 }
 
-
 impl Capture {
     pub fn new() -> Self {
         Self {
             running: false,
             sniffer: Default::default(),
-            tree : create_tree(),
+            tree: create_tree(),
         }
     }
     pub fn get_packets(&mut self) {
@@ -104,12 +116,12 @@ impl Capture {
         }
     }
     pub fn ui(&mut self, ui: &mut Ui) {
-        if ui.button("Start").clicked() {
+        /*if ui.button("Start").clicked() {
             self.start();
         }
         if ui.button("Stop").clicked() {
             self.stop();
-        }
+        }*/
 
         /*ScrollArea::vertical().max_height(500.0).show(ui, |ui| {
             self.table.render(ui, &mut self.sniffer.captured_packets);
@@ -164,6 +176,7 @@ fn create_tree() -> egui_tiles::Tree<Pane> {
     egui_tiles::Tree::new(root, tiles)
 }
 
+#[derive(Clone)]
 pub struct PacketTable {
     striped: bool,
     resizable: bool,
@@ -184,7 +197,7 @@ impl PacketTable {
 }
 
 impl PacketTable {
-    pub fn render(&mut self, ui: &mut egui::Ui, data: &mut Vec<EthernetFrame>) {
+    pub fn render(&mut self, ui: &mut egui::Ui, data: &Vec<EthernetFrame>) {
         use egui_extras::{Column, TableBuilder};
         let mut table = TableBuilder::new(ui)
             .striped(self.striped)
@@ -261,10 +274,11 @@ impl PacketTable {
     }
 }
 
-pub enum Module{
+#[derive(Clone)]
+pub enum Module {
     Packets(PacketTable),
     PacketDrill(Drilldown),
-    Payload
+    Payload,
 }
 
 fn custom_panic_handler(info: &panic::PanicInfo) {
